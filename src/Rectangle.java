6c91b429802227c990652d84cd881fe491bb9a89
Harsh Patil <itsharshhh6@gmail.com>
2025-12-05 17:14:10 +0530

    Rectangle

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows : ");
+        int r = sc.nextInt();
+        System.out.println("Enter columns : ");
+        int c = sc.nextInt();
+        for(int i = 0; i < r; i++){
+            for (int j = 0; j < c; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
