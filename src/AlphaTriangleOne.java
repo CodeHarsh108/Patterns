15f2b01c5cd389da524d52431253ab0fbd35275a
Harsh Patil <itsharshhh6@gmail.com>
2025-12-12 09:44:44 +0530

    AlphaTriangleOne

---
+++ src/AlphaTriangleOne.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AlphaTriangleOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n : ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            char alpha = (char) ((char) 65  + i);
+            for (int j = 0; j <= i; j++){
+                System.out.print(alpha + " ");
+            }
+            for (int j = 0; j < n - i - 1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
