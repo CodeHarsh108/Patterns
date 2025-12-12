c62c852a53d261b9eb6917e42559a610efbafbe5
Harsh Patil <itsharshhh6@gmail.com>
2025-12-12 08:47:39 +0530

    Hour Glass

---
+++ src/HourGlass.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class HourGlass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Dimension : ");
+        int n = sc.nextInt();
+
+        // Top half: Inverted Pyramid
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Bottom half: Upright Pyramid
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
